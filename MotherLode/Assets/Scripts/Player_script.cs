56572c4ba9e3c8ef5e173c7f8d9a695b4b282e11
DavidRMincer <david.r.mincer@live.com>
2021-03-12 18:42:51 +0000

    Models Imported, Player Movement Started

---
+++ MotherLode/Assets/Scripts/Player_script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_script : Character_script
+{
+    private void Update()
+    {
+        // Acceleration
+        if (Input.GetButton("Fire1"))
+        {
+            Accelerate();
+        }
+
+        // Rotate
+        Vector2 newRotate = Vector2.zero;
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            newRotate.y = Input.GetAxis("Horizontal") * rotateSpeed;
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            newRotate.x = -Input.GetAxis("Vertical") * rotateSpeed;
+        }
+
+        Rotate(newRotate.x, newRotate.y);
+
+        // Roll
+        Roll(Input.GetAxis("Roll"));
+        //if (Input.GetAxis("Roll") != 0)
+        //{
+        //    Roll(Input.GetAxis("Roll"));
+        //}
+    }
+}
