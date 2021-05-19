948771408d0834f38d7918dcbb2cb1989fc6ec1e
DavidRMincer <david.r.mincer@live.com>
2021-05-19 22:35:28 +0100

    DIY Lerp Started

--- Mother_Lode/Assets/Scripts/AssetMovement_script.cs
+++ Mother_Lode/Assets/Scripts/AssetMovement_script.cs
@@ -20,20 +20,32 @@ public class AssetMovement_script : Action_script
         StartCoroutine(MoveAssetIEnum());
     }
 
+    /// <summary>
+    /// Lerp asset to new transform
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator MoveAssetIEnum()
     {
         float counter = 0;
         Transform prevTransform = asset.transform;
-
+        Vector3 distance = newTransform.position - asset.transform.position;
+        
         do
         {
             counter = (counter + Time.deltaTime > duration) ? duration : counter + Time.deltaTime;
+            float time = curve.Evaluate(counter / duration);
+
+            Vector3 newPos = prevTransform.position + (distance * time);
+
+            Debug.Log(counter / duration);
+
+            asset.transform.position = newPos;
+
+            //asset.transform.position = Vector3.Lerp(prevTransform.position, newTransform.position, curve.Evaluate(counter / duration));
+            //asset.transform.rotation = Quaternion.Lerp(prevTransform.rotation, newTransform.rotation, curve.Evaluate(counter / duration));
+
             yield return new WaitForSeconds(Time.deltaTime);
 
-            asset.transform.position = Vector3.Lerp(prevTransform.position, newTransform.position, curve.Evaluate(counter / duration));
-            asset.transform.rotation = Quaternion.Lerp(prevTransform.rotation, newTransform.rotation, curve.Evaluate(counter / duration));
-
-            Debug.Log("Counter: " + curve.Evaluate(counter / duration));
         } while (counter < duration);
     }
 }
