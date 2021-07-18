fe7f055eaa1e63631cd7df249491ae6aef9f3a29
DavidRMincer <david.r.mincer@live.com>
2021-07-18 16:24:45 +0100

    Explosion

---
+++ Mother_Lode/Assets/Scripts/ExplosionAction_script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionAction_script : Action_script
+{
+    public ShipExplosion_script explosion_Script;
+
+    public override void Act()
+    {
+        explosion_Script.Explode();
+    }
+}
---
+++ Mother_Lode/Assets/Scripts/ShipExplosion_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipExplosion_script : MonoBehaviour
+{
+    public GameObject shipMesh;
+    public ParticleSystem particles;
+
+    public void Explode()
+    {
+        StartCoroutine(ExplosionIEnum());
+    }
+
+    private IEnumerator ExplosionIEnum()
+    {
+        shipMesh.SetActive(false);
+        particles.Play();
+
+        yield return new WaitForSeconds(3);
+
+        shipMesh.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
